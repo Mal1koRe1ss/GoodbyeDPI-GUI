00864c9d6bfcb7aa32b54dd3d2069bcad603bec0
Yavuz Selim <pounter@post.com>
2025-04-05 22:13:36 +0300

    ~Settings menu adjustments

--- src/net/gdpi/gui/SettingsGui.java
+++ src/net/gdpi/gui/SettingsGui.java
@@ -1,9 +1,15 @@
 package net.gdpi.gui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+
 import com.formdev.flatlaf.FlatDarkLaf;
 import net.gdpi.handlers.ConfigHandler;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class SettingsGui extends JDialog {
 
@@ -16,6 +22,7 @@ public class SettingsGui extends JDialog {
 
     private JCheckBox customRedirCheckBox;
     private JTextField customParamField;
+    private JTextField serviceParamField;
 
     private ConfigHandler configHandler;
 
@@ -32,12 +39,12 @@ public class SettingsGui extends JDialog {
         setLocationRelativeTo(null);
 
         configHandler = new ConfigHandler();
-        loadConfig(); // Mevcut ayarları yükle
+        loadConfig();
     }
 
     private void setupFrame() {
         setTitle("Settings");
-        setSize(400, 400);
+        setSize(500, 500);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLayout(new BorderLayout());
         setResizable(false);
@@ -56,7 +63,7 @@ public class SettingsGui extends JDialog {
         generalPanel.setLayout(new BorderLayout());
         generalPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         
-        // Genel ayarlar içeriği
+        // General Setting Content
         JPanel content = new JPanel();
         content.add(new JLabel("General Application Settings"));
         generalPanel.add(content, BorderLayout.CENTER);
@@ -67,29 +74,76 @@ public class SettingsGui extends JDialog {
     private void customizeTab() {
         customizePanel = new JPanel();
         customizePanel.setLayout(new BorderLayout());
-        customizePanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        customizePanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
-        // DNS Özelleştirme Paneli
+        // DNS Adjustment Panel
         JPanel dnsPanel = new JPanel();
         dnsPanel.setLayout(new BoxLayout(dnsPanel, BoxLayout.Y_AXIS));
         dnsPanel.setBorder(BorderFactory.createTitledBorder("Custom configuration"));
 
+        JPanel checkboxPanel = new JPanel();
+        checkboxPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         customRedirCheckBox = new JCheckBox("Enable Custom DNS Redir");
-        customRedirCheckBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+        checkboxPanel.add(customRedirCheckBox);
 
         customParamField = new JTextField(20);
         customParamField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
-        customParamField.setToolTipText("Format: --dns-server=1.1.1.1");
+        String fieldText_1 = "Format: --dns-server=1.1.1.1";
+        customParamField.setToolTipText(fieldText_1);
+        addPlaceHolder(customParamField, fieldText_1);
+
+        JPanel paramPanel = new JPanel();
+        paramPanel.setLayout(new GridLayout(1, 2));
+        paramPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        JLabel paramLabel = new JLabel("DNS Parameters:");
+        paramPanel.add(paramLabel, BorderLayout.WEST);
+        paramPanel.add(customParamField, BorderLayout.CENTER);
+
+        serviceParamField = new JTextField(20);
+        serviceParamField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+        String fieldText_2 = "Format: --dns-server=1.1.1.1";
+        serviceParamField.setToolTipText(fieldText_2);
+        addPlaceHolder(serviceParamField, fieldText_2);
+
+        JPanel servicePanel = new JPanel();
+        servicePanel.setLayout(new GridLayout(1, 2));
+        servicePanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        JLabel serviceLabel = new JLabel("Service Parameters:");
+        servicePanel.add(serviceLabel, BorderLayout.WEST);
+        servicePanel.add(serviceParamField, BorderLayout.CENTER);
 
         dnsPanel.add(Box.createVerticalStrut(5));
-        dnsPanel.add(customRedirCheckBox);
-        dnsPanel.add(Box.createVerticalStrut(10));
-        dnsPanel.add(new JLabel("Custom DNS Parameters:"));
-        dnsPanel.add(customParamField);
+        dnsPanel.add(checkboxPanel);
+        dnsPanel.add(Box.createVerticalStrut(5));
+        dnsPanel.add(paramPanel);
+        dnsPanel.add(Box.createVerticalStrut(5));
+        dnsPanel.add(servicePanel);
         dnsPanel.add(Box.createVerticalGlue());
 
         customizePanel.add(dnsPanel, BorderLayout.NORTH);
         tabbedPane.addTab("Customize", customizePanel);
+    }
+
+    private void addPlaceHolder(JTextField field, String text) {
+        field.setText(text);
+        field.setForeground(Color.GRAY);
+        field.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (field.getText().equals(text)) {
+                    field.setText("");
+                    field.setForeground(Color.WHITE);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (field.getText().isEmpty()) {
+                    field.setText(text);
+                    field.setForeground(Color.GRAY);
+                }
+            }
+        });
     }
 
     private void createButtons() {
@@ -112,7 +166,6 @@ public class SettingsGui extends JDialog {
     }
 
     private void loadConfig() {
-        // ConfigHandler'dan ayarları yükle
         customRedirCheckBox.setSelected(MainGui.customRedir);
         // customParamField.setText(configHandler.getCustomDnsParams());
     }
@@ -122,7 +175,7 @@ public class SettingsGui extends JDialog {
         boolean customService = customRedirCheckBox.isSelected();
         String customParam = customParamField.getText();
 
-        // ConfigHandler'a kaydet
+        // Save ConfigHandler
         configHandler.saveConfig(customRedir, customService, customParam);
         MainGui.customRedir = customRedir;
         MainGui.customService = customService;
